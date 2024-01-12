1334c758571ab8486e91d40450974d5f100db0a6
vo_o <vo_o@etna-alternance.net>
2024-01-12 12:33:07 +0100

    [ADD] => create new canva

---
+++ game-portfolio/src/ressoources.js
@@ -0,0 +1,16 @@
+class Ressources {
+  constructor() {
+    // All assets to download
+    this.toLoad = {
+      sky: "/sprites/sky.png",
+      ground: "/sprites/ground.png",
+      hero: "/sprites/hero-sheet"
+    };
+    // Place to stack all images
+    this.images = {};
+
+    Object.keys(this.toLoad).forEach(key => {
+
+    })
+  }
+}
