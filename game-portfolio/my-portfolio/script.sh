a52dbf2234ccbef86800b01becdf237fda512451
vo_o <vo_o@etna-alternance.net>
2024-01-12 12:49:58 +0100

    [ADD] => setting up projects

---
+++ game-portfolio/my-portfolio/script.sh
@@ -0,0 +1,16 @@
+cd src
+mkdir assets
+mkdir components
+cd components
+mkdir atoms && touch atoms/index.ts
+mkdir molecules && touch molecules/index.ts
+mkdir organisms && touch organisms/index.ts
+cd ..
+mkdir entities
+mkdir navigation
+mkdir screens
+mkdir services
+mkdir styles
+mkdir translations
+mkdir utils
+
---
+++ game-portfolio/my-portfolio/src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/App.css";
+
+export const App = () => {
+  return (
+    <>
+      <div className="title">Hi, I'm VO OLIVIER</div>
+    </>
+  );
+};
