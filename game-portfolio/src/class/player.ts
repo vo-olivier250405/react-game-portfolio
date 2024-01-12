8eb32db4f2d07df3f9f88049d994e69731c8cfb2
vo_o <vo_o@etna-alternance.net>
2024-01-12 16:58:37 +0100

    [ADD] => adding the player class

---
+++ game-portfolio/src/class/player.ts
@@ -0,0 +1,33 @@
+export class Player {
+  position: { x: number; y: number };
+  width: number;
+  height: number;
+  sides: { bottom: number };
+
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+    this.sides = { bottom: this.position.y + this.height };
+  }
+
+  draw(canvasSurface: CanvasRenderingContext2D) {
+    canvasSurface.fillStyle = "blue";
+    canvasSurface.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update(canvas: HTMLCanvasElement) {
+    if (this.sides.bottom < canvas.height) {
+      this.position.y++;
+      this.sides.bottom = this.position.y + this.height;
+    }
+  }
+}
--- game-portfolio/src/main.ts
+++ game-portfolio/src/main.ts
@@ -1,51 +1,28 @@
 import "./style.css";
-import { playerProps, Player } from "./interfaces/player";
+import { Player } from "./class";
 
 const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
 
-const canvas_surface: CanvasRenderingContext2D = canvas.getContext("2d")!;
+const canvasSurface: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
 canvas.width = 64 * 64;
 canvas.height = 64 * 9;
 
 // init a player
-const player: Player = {
-  position: { x: 100, y: 100 },
-  width: 100,
-  height: 100,
-  draw() {
-    canvas_surface.fillStyle = "blue";
-    canvas_surface.fillRect(
-      player.position.x,
-      player.position.y,
-      player.width,
-      player.height
-    );
-  },
-};
-
-const clearRectWhileAnimate = (ref: CanvasRenderingContext2D) => {
-  ref.clearRect(0, 0, canvas.width, canvas.height);
-};
-
-let bottom = player.position.y + player.height;
-
-export const gravity = (object: Player) => {
-  if (bottom < canvas.height) {
-    object.position.y++;
-    bottom = player.position.y + player.height;
-  }
-};
+const player = new Player();
 
 // animation loop
 export const animatePlayer = () => {
   /**Redraw player rect infinitely */
   window.requestAnimationFrame(animatePlayer);
 
-  canvas_surface.fillStyle = "red";
-  canvas_surface.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
-  gravity(player);
+  // background
+  canvasSurface.fillStyle = "red";
+  canvasSurface.fillRect(0, 0, canvas.width, canvas.height);
+
+  // player
+  player.draw(canvasSurface);
+  player.update(canvas);
 };
 
 animatePlayer();
